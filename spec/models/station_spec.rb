3ba4392bcaee89fbe4c6058c18e78b130df3cfa0
nico24687 <theonlynico@hotmail.com>
2018-02-07 10:27:50 -0700

    model test for a station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,5 +2,12 @@ require 'rails_helper'
 
 describe Station do 
   it "contains the required information" do 
+    station = Station.new(fuel_type_code: "random fuel", access_days_time:"never enter", station_name: "random name", station_address: "random address", distance: "random distance")
+    
+    expect(station.fuel_type_code).to eq("random fuel")
+    expect(station.access_days_time).to eq("never enter")
+    expect(station.station_name).to eq("random name")
+    expect(station.station_address).to eq("random address")
+    expect(station.distance).to eq("random distance")
   end
 end
