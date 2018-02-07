87edf8943edcd86f80bd76d224e873c674ceb003
nico24687 <theonlynico@hotmail.com>
2018-02-07 10:33:59 -0700

    now finding stations within 6 miles

--- app/services/get_stations_info_service.rb
+++ app/services/get_stations_info_service.rb
@@ -2,7 +2,7 @@ class GetStationsInfoService
   def perform(zipcode)
     api_key = ENV['nrel_key']
 
-    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?&api_key=#{api_key}&location=#{zipcode}&limit=10&fuel_type=ELEC,LPG")
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?&api_key=#{api_key}&location=#{zipcode}&limit=10&fuel_type=ELEC,LPG&radius=6.0")
 
     body = JSON.parse(response.body)
 
